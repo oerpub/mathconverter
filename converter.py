f89bc19cc69190b053d0b732545a77b505cd22c8
Marvin Reimer <therealmarv@gmail.com>
2016-11-18 14:47:46 -0600

    give MathML Cloud results

--- converter.py
+++ converter.py
@@ -12,6 +12,7 @@ output_formats = ['latex', 'mathml']
 def mathml2latex_yarosh(equation):
     pass
 
+
 def _call_mathml_cloud(equation, mathtype):
     try:
         resp = requests.post('https://api.mathmlcloud.org/equation',
@@ -21,10 +22,13 @@ def _call_mathml_cloud(equation, mathtype):
             mathml = data['components'][0]['source']
             return mathml
         except IndexError:
-            return 'null'   # bug in MathML Cloud. It sometimes returns 200 but no result (bummer).
+            # bug in MathML Cloud. It sometimes returns 200 but no result
+            # (bummer).
+            return 'null'
     except Exception, err:
         sys.stderr.write('MathML Cloud ERROR: %sn' % str(err))
         sys.exit(2)
+
 
 def mathmlcloud2mathml(equation, iformat):
     #    resp = requests.post('https://api.mathmlcloud.org/equation',{'math': equation,'mathType':'TeX','mml':'true', 'svg':'true', 'png':'true', 'description':'true'})
@@ -33,7 +37,7 @@ def mathmlcloud2mathml(equation, iformat):
         mathtype = 'AsciiMath'
     elif iformat == 'mathml':
         mathtype = 'mml'
-    # try x times to get MathML from web service:
+    # try x times to get MathML cloud result:
     for i in range(1, 5):
         # print "Attempt calling MathML Cloud Server: {}".format(i)
         mathml = _call_mathml_cloud(equation, mathtype)
@@ -44,6 +48,7 @@ def mathmlcloud2mathml(equation, iformat):
         sys.exit(2)
     return mathml
 
+
 @click.command()
 @click.option('--equation', prompt='Equation', help='Equation to convert')
 @click.option('--iformat', type=click.Choice(input_formats), prompt='Input Format', help='Equation input format')
@@ -51,10 +56,15 @@ def mathmlcloud2mathml(equation, iformat):
 def convert(equation, iformat, oformat):
     print
     if (iformat == oformat):
-        sys.stderr.write('Nothing to convert here. Input and output format are the same!')
+        sys.stderr.write(
+            'Nothing to convert here. Input and output format are the same!')
         sys.exit(1)
-    # click.echo('Equation %s' % equation)
-    print mathmlcloud2mathml('x*2', 'asciimath')
+    if (oformat == 'mathml'):
+        result = mathmlcloud2mathml(equation, iformat)
+        click.echo(result)
+    else:
+        print "t.b.a."
+
 
 if __name__ == '__main__':
     convert()
