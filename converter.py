9be4d2a4867ff04003c78386039d58cb1720e459
Marvin Reimer <therealmarv@gmail.com>
2016-11-18 11:08:03 -0600

    no asciimath output

--- converter.py
+++ converter.py
@@ -4,13 +4,13 @@
 import click
 import sys
 
-math_formats = ['asciimath', 'latex', 'mathml']
-
+input_formats = ['asciimath', 'latex', 'mathml']
+output_formats = ['latex', 'mathml']
 
 @click.command()
 @click.option('--equation', prompt='Equation', help='Equation to convert')
-@click.option('--iformat', type=click.Choice(math_formats), prompt='Input Format', help='Equation input format')
-@click.option('--oformat', type=click.Choice(math_formats), prompt='Output Format', help='Equation output format')
+@click.option('--iformat', type=click.Choice(input_formats), prompt='Input Format', help='Equation input format')
+@click.option('--oformat', type=click.Choice(output_formats), prompt='Output Format', help='Equation output format')
 def convert(equation, iformat, oformat):
     if (iformat == oformat):
         print "Nothing to convert here. Input and output format are the same!"
