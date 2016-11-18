88235233701bed138147b97e3d658e817d872906
Marvin Reimer <therealmarv@gmail.com>
2016-11-18 11:00:27 -0600

    ignore same input and output format

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import click
+import sys
 
 math_formats = ['asciimath', 'latex', 'mathml']
 
@@ -11,6 +12,9 @@ math_formats = ['asciimath', 'latex', 'mathml']
 @click.option('--iformat', type=click.Choice(math_formats), prompt='Input Format', help='Equation input format')
 @click.option('--oformat', type=click.Choice(math_formats), prompt='Output Format', help='Equation output format')
 def convert(equation, iformat, oformat):
+    if (iformat == oformat):
+        print "Nothing to convert here. Input and output format are the same!"
+        sys.exit(1)
     click.echo('Equation %s!' % equation)
 
 if __name__ == '__main__':
